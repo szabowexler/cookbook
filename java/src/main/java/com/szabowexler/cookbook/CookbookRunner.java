1be0742e127f7bcaf80354e54c2fde8a033e2287
Elias Szabo-Wexler <eszabowexler@hubspot.com>
2016-04-09 18:34:50 -0400

    Write tex out to parallel dir structure

--- java/src/main/java/com/szabowexler/cookbook/CookbookRunner.java
+++ java/src/main/java/com/szabowexler/cookbook/CookbookRunner.java
@@ -1,6 +1,7 @@
 package com.szabowexler.cookbook;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -8,6 +9,7 @@ import java.util.stream.Collectors;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.google.common.io.Files;
 import com.google.inject.Guice;
 import com.szabowexler.cookbook.recipes.RecipeParser;
 import com.szabowexler.cookbook.recipes.RecipeTexGenerator;
@@ -32,7 +34,20 @@ public class CookbookRunner {
     }
 
     String tex = RecipeTexGenerator.generateRecipeTex(RecipeParser.parse(f));
-    LOG.info("Generated:\n{}", tex);
+    try {
+      writeTex(f, tex);
+    } catch (IOException ex) {
+      LOG.error("Unable to write tex file for {}", f.getAbsolutePath(), ex);
+    }
+  }
+
+  private static void writeTex(File originalRecipeFile, String tex) throws IOException {
+    File texFile = new File(
+        originalRecipeFile.getAbsolutePath()
+                          .replace("/recipies/", "/tex/")
+                          .replace(".recipe", ".tex"));
+    Files.createParentDirs(texFile);
+    Files.write(tex.getBytes(), texFile);
   }
 
   public static void main(String[] args) {
